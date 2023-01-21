f7837259871fcb28f5d1092c657f7447e656e5d1
Chris Clarke <chris.clarke7878@outlook.com>
2023-01-21 17:50:23 -0500

    Pass all the tests up to 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitString = string.split('');
+    let reveresedString = [];
+    let countdown = string.length - 1;
 
+    for (let i = 0; i < string.length; i++) {
+        reveresedString[i] = splitString[countdown];
+        countdown--;
+    }
+    return reveresedString.join("");
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(array, ...toRemove) {
+    let newArray = array;
+    for (let y = 0; y < toRemove.length; y++) {
+        for (let i = 0; i < newArray.length; i++) {
+            if (newArray[i] === toRemove[y]) {
+                newArray.splice(i, 1);
+                i = -1;
+            }
+        }
+    }
+    return newArray;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
